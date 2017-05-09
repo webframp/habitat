dc0b4696aed70f565cabc39df7744c7d2f988886
David Wrede <dwrede@chef.io>
2017-05-09 11:11:33 -0700

    Fixed functionality breaks + updated content
    Signed-off-by: David Wrede <dwrede@chef.io>

--- www/source/javascripts/try.js
+++ www/source/javascripts/try.js
@@ -27,6 +27,18 @@ $("#mobile-keyboard-trigger").click(function() {
         return $.get("/try/responses/" + name + ".txt");
     }
 
+    function getExample(name, callback) {
+        getResponse(name).then(function (txt) {
+                        inStudio = false;
+                        callback(format(txt));
+                        shell.setPrompt(rootPrompt);
+                        shell.onNewPrompt(function(callback) {
+                            promptCounter += 1;
+                            callback(rootPrompt);
+                        });
+        });
+    }
+
     function success(display) {
         $("#success .button").removeClass("secondary").addClass("cta");
         if(display === "hide") {
@@ -100,6 +112,22 @@ $("#mobile-keyboard-trigger").click(function() {
         }
     });
 
+    shell.setCommandHandler("curl", {
+         exec: function(cmd, args, callback) {
+     
+            if (args.join(" ") === "http://172.17.0.2:9631/services/postgresql/default/health")
+            {
+               getExample("hab-health-check", callback);
+
+                  if (step === 6) { success(); }
+                } else {
+                  getResponse("hab-sup-config-help").then(function (txt) {
+                      callback(format(txt));
+                  });
+                }
+            }
+    });
+
     // hab commands
     shell.setCommandHandler("hab", {
         exec: function(cmd, args, callback) {
@@ -107,48 +135,45 @@ $("#mobile-keyboard-trigger").click(function() {
           switch(args[0]) {
              case "start":
                    // Start a service
-                  if (args[1] === "example/ruby-rails-sample") {
-                    getResponse("hab-start-service").then(function (txt) {
-                        inStudio = false;
-                        callback(format(txt));
-                        shell.setPrompt(rootPrompt);
-                        shell.onNewPrompt(function(callback) {
-                            promptCounter += 1;
-                            callback(rootPrompt);
-                        });
+                  if (args.join(" ") ===  "start example/ruby-rails-sample") { 
+
+                    getExample("hab-start-service", callback);
+
+                    // step is pulled from the progress bar attribute in the DOM
+                    if (step === 1) { success(); }
+                  } else if (args.join(" ") ===  "start example/ruby-rails-sample --peer 172.17.0.2 --bind database:postgresql.default") {
+
+                      getExample("hab-bind", callback);
                         
-                        if (step === 1) { success(); }
-                    });
-                  } else if (args[1] + ' ' + args[2] === "--peer 172.17.0.2 --bind database:postgresql.default") {
-                    getResponse("hab-bind").then(function (txt) {
-                        inStudio = false;
-                        callback(format(txt));
-                        shell.setPrompt(rootPrompt);
-                        shell.onNewPrompt(function(callback) {
-                            promptCounter += 1;
-                            callback(rootPrompt);
+                      if (step === 2) { success(); }
+
+                  } else if (args.join(" ") ===  "start core/postgresql -t leader") {
+
+                      getExample("hab-start-first-node", callback);
+                      
+                      if (step === 4) { success(); }
+                            
+                     } else if (args.join(" ") ===  "start core/postgresql -t leader --peer 172.17.0.2") {
+
+                           getExample("hab-start-additional-node", callback);
+                           if (step === 5) { success(); }
+
+                           $(".node-status").html("connected").parent().addClass("updated");
+                           $(".button-badge, .full-output").show();
+
+                     } else {
+                        getResponse("hab-start-help").then(function (txt) {
+                            callback(format(txt));
                         });
-                        // step is pulled from the progress bar attribute in the DOM
-                        if (step === 2) { success(); }
-                     });
-                  } else
-                    getResponse("hab-start-help").then(function (txt) {
-                        callback(format(txt));
-                  });
+                  }
                   break;
              case "config":
                 // Apply service group configuration
                 // inject the config.toml into the group
-                if (args.join(" ") === "config apply --peer 172.17.0.2 ruby-rails-sample.default 1 fix_db_conn.toml") {
-                  getResponse("hab-config-apply").then(function (txt) {
-                      inStudio = false;
-                      callback(format(txt));
-                      shell.setPrompt(rootPrompt);
-                      shell.onNewPrompt(function(callback) {
-                          promptCounter += 1;
-                          callback(rootPrompt);
-                      });
-                      // step is pulled from the progress bar attribute in the DOM
+                if (args.join(" ") === "config apply --peer 172.17.0.2 ruby-rails-sample.default 1 update_config.toml") {
+                 
+                      getExample("hab-config-apply", callback);
+
                       if (step === 3) {
                         success();
 
@@ -158,11 +183,24 @@ $("#mobile-keyboard-trigger").click(function() {
                         $(".node-status").html("change applied").parent().addClass("updated");
                         $(".button-badge, .full-output").show();
                       }
-                  });
+      
                 } else 
                    getResponse("hab-config-apply-help").then(function (txt) {
                        callback(format(txt));
                    });
+                break;
+             case "sup":
+                // Find out the status of the service from the supervisor
+                if (args.join(" ") === "sup status core/postgresql") {
+                  
+                  getExample("hab-monitor", callback);
+
+                  if (step === 6) { success(); }
+                } else {
+                  getResponse("hab-sup-config-help").then(function (txt) {
+                      callback(format(txt));
+                  });
+                }
                 break;
              default:
                // the user entered `hab` followed by an unsupported subcommand let's show them `hab help` for a list of available emulator subcommands and link to docs for the full subcommand list
@@ -173,84 +211,6 @@ $("#mobile-keyboard-trigger").click(function() {
                        "for a more complete liste of features.<br><br>");
                });
           }
-        //     // hab start [subcommand]
-        //     if (args[0] === "blarg") {
-
-        //         } else if (args[1] + ' ' + args[2] === "--peer 172.17.0.2 --bind database:postgresql.default") {
-        //           getResponse("hab-bind").then(function (txt) {
-        //               inStudio = false;
-        //               callback(format(txt));
-        //               shell.setPrompt(rootPrompt);
-        //               shell.onNewPrompt(function(callback) {
-        //                   promptCounter += 1;
-        //                   callback(rootPrompt);
-        //               });
-        //               // step is pulled from the progress bar attribute in the DOM
-        //               if (step === 2) { success(); }
-        //           });
-
-        //         // Adding a leader/follower topology
-        //         } else if (args[1] + ' ' + args[2] === "-t leader") {
-        //           if (args[3] === 'core/redis') {
-
-        //             // adding the first/leader node
-        //             if ((args[4] == null) && (step === 6)) {
-        //               getResponse("hab-start-first-node").then(function (txt) {
-        //                   inStudio = true;
-        //                   callback(format(txt));
-        //                   shell.setPrompt(rootPrompt);
-        //                   shell.onNewPrompt(function(callback) {
-        //                       promptCounter += 1;
-        //                       callback(rootPrompt);
-        //                   });
-
-        //                   if (step === 6) { success('hide'); }
-        //               });
-
-        //             // adding an additional/follower node
-        //             } else if (args[4] + ' ' + args[5] === "--peer 172.17.0.4") {
-        //               getResponse("hab-start-additional-node").then(function (txt) {
-        //                   inStudio = true;
-        //                   callback(format(txt));
-        //                   shell.setPrompt(rootPrompt);
-        //                   shell.onNewPrompt(function(callback) {
-        //                       promptCounter += 1;
-        //                       callback(rootPrompt);
-        //                   });
-        //                   //change button text to reflect resulting status
-        //                   //show badge on window buttons
-        //                   //show full updated cli output
-        //                   if (step === 7) {
-        //                     success();
-
-        //                     $(".node-status").html("connected").parent().addClass("updated");
-        //                     $(".button-badge, .full-output").show();
-        //                   }
-        //               });
-
-        //             // they could be on step 6 or 7 since the command/subcommand is the same
-        //             } else if (step === 7) {
-        //               getResponse("hab-follower-help").then(function (txt) {
-        //                   callback(format(txt));
-        //               });
-
-        //             } else {
-        //               getResponse("hab-leader-help").then(function (txt) {
-        //                   callback(format(txt));
-        //               });
-        //             };
-        //           } else {
-        //             getResponse("hab-leader-help").then(function (txt) {
-        //                 callback(format(txt));
-        //             });
-        //           }
-        //         // if user tries to start anything else, then show the 'hab start' help
-        //         } else {
-        //           getResponse("hab-start-help").then(function (txt) {
-        //               callback(format(txt));
-        //           });
-        //         };
-        // }
        },
     });
     shell.setPrompt(rootPrompt);
